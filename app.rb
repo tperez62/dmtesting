f983de29f32958ceb1c18aa951243741006f1f2d
tperez62 <perezt1994@gmail.com>
2015-10-08 15:32:27 -0400

    Removed Comment

--- app.rb
+++ app.rb
@@ -3,7 +3,5 @@ require './models/Tree'
 require './models/Apple'
 require './models/Seed'
 
-#Changing the comment
-
 DataMapper.setup :default, "sqlite3://#{Dir.pwd}/dm.db"
 DataMapper.auto_upgrade!
