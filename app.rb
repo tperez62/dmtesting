ccf780db5fdab18afacb97ac5e9556522041df3e
tperez62 <perezt1994@gmail.com>
2015-10-08 15:47:59 -0400

    Revert "Removed Comment"
    This reverts commit f983de29f32958ceb1c18aa951243741006f1f2d.

--- app.rb
+++ app.rb
@@ -3,5 +3,7 @@ require './models/Tree'
 require './models/Apple'
 require './models/Seed'
 
+#Changing the comment
+
 DataMapper.setup :default, "sqlite3://#{Dir.pwd}/dm.db"
 DataMapper.auto_upgrade!
