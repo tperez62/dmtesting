0cc492912c2bb90fb8aa392a51f85298ec91fe3f
tperez62 <perezt1994@gmail.com>
2015-10-08 15:30:49 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -3,5 +3,7 @@ require './models/Tree'
 require './models/Apple'
 require './models/Seed'
 
+#Adding this comment on github
+
 DataMapper.setup :default, "sqlite3://#{Dir.pwd}/dm.db"
 DataMapper.auto_upgrade!
